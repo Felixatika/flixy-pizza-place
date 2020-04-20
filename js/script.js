f56aaccfb129bcc1d5415558b97b7ef7de6f5ae6
Felix <felixatika@gmail.com>
2020-04-20 10:56:27 -0400

    add functionalities of buttons in js

--- js/script.js
+++ js/script.js
@@ -134,7 +134,54 @@ $(document).ready(function () {
           newOrder.total +
           "</td></tr>"
       );
+      console.log(newOrder);
     });
+    // Checkout button
+    $("button#checkout").click(function(){ 
+      $("button#checkout").hide();
+      $("button.addPizza").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      console.log("Your total bills is sh. "+checkoutTotal);
+      $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
+    });
+//home delivery button
+    $("button.deliver").click(function () {
+      $(".pizzatable").hide();
+      $(".order-ready h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let deliveryAmount = checkoutTotal + 100;
+      console.log("You will pay sh. " + deliveryAmount + " on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: " + deliveryAmount);
+
+    });
+    //when one clicks place order button
+    $("button#final-order").click(function (event) {
+      event.preventDefault();
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliveryAmount = checkoutTotal + 100;
+      console.log("Final Bill is: " + deliveryAmount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name")).val() && $("input#phone").val && $("input#location").val() != ""){
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+      }
+    })
   });
 });
 
